8306b20b8d96e69772e0f2c6da288aed16c13c37
Ali <nf138992@gmail.com>
2025-05-01 23:59:23 +0330

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,26 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import translationEN from './locales/en.json';
-import translationFA from './locales/fa.json';
+import en from './i18n/locales/en.json';
+import fa from './i18n/locales/fa.json';
+import ar from './i18n/locales/ar.json';
+import tr from './i18n/locales/tr.json';
+import ru from './i18n/locales/ru.json';
+import zh from './i18n/locales/zh.json';
+import de from './i18n/locales/de.json';
+import es from './i18n/locales/es.json';
+import hi from './i18n/locales/hi.json';
 
 const resources = {
-  en: { translation: translationEN },
-  fa: { translation: translationFA }
+  en: { translation: en },
+  fa: { translation: fa },
+  ar: { translation: ar },
+  tr: { translation: tr },
+  ru: { translation: ru },
+  zh: { translation: zh },
+  de: { translation: de },
+  es: { translation: es },
+  hi: { translation: hi }
 };
 
 i18n
