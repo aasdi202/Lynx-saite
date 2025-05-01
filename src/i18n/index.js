6a2b85fc069ec80ca5ef7527415d9941230f2ffa
Ali <nf138992@gmail.com>
2025-05-01 16:16:13 +0330

    Update index.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,28 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-
 import translationEN from './locales/en.json';
 import translationFA from './locales/fa.json';
 
 const resources = {
-  en: {
-    translation: translationEN
-  },
-  fa: {
-    translation: translationFA
-  }
+  en: { translation: translationEN },
+  fa: { translation: translationFA }
 };
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
+    lng: 'en', // زبان پیش‌فرض
     fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false
-    }
+    interpolation: { escapeValue: false }
   });
 
 export default i18n;
