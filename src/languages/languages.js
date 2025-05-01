c182a031e665a464a92ac2ef331f367dfa8498bd
Ali <nf138992@gmail.com>
2025-05-01 17:08:54 +0330

    Update and rename placeholder.txt to languages.js

---
+++ src/languages/languages.js
@@ -0,0 +1,11 @@
+export const languages = [
+  { code: "en", name: "English", flag: "gb" },
+  { code: "fa", name: "فارسی", flag: "ir" },
+  { code: "zh", name: "中文", flag: "cn" },
+  { code: "ru", name: "Русский", flag: "ru" },
+  { code: "tr", name: "Türkçe", flag: "tr" },
+  { code: "ar", name: "العربية", flag: "sa" },
+  { code: "es", name: "Español", flag: "es" },
+  { code: "de", name: "Deutsch", flag: "de" },
+  { code: "hi", name: "हिंदी", flag: "in" },
+];
