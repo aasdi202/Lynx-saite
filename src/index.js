96612f12808d1e9d889a2ca220d70361d0ccaee6
Ali <nf138992@gmail.com>
2025-05-02 00:38:18 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import './i18n';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
-import './i18n'; 
+import './index.css'; 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
