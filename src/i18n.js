7a6686a943c0a9d8527bad79655bab765b60e9d7
Ali <nf138992@gmail.com>
2025-05-02 22:56:43 +0330

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -4,18 +4,17 @@ import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  .use(HttpBackend) // لود ترجمه از public/locales/
-  .use(LanguageDetector) // تشخیص زبان کاربر
-  .use(initReactI18next) // اتصال به ری‌اکت
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
     fallbackLng: 'en',
     debug: false,
-    supportedLngs: ['en', 'fa', 'ar', 'tr', 'ru', 'zh', 'de', 'es', 'hi'],
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json', // مسیر فایل ترجمه
-    },
     interpolation: {
       escapeValue: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json',
     },
   });
 
