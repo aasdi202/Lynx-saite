27f576870174b6664f3c122eb08b63388beca6ee
Ali <nf138992@gmail.com>
2025-05-02 22:37:28 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Roadmap from "./pages/Roadmap";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import ScrollToTop from "./components/layout/ScrollToTop";
-
-function App() {
-  return (
-    <Router>
-      <ScrollToTop />
-      <Header />
-      <main className="min-h-screen bg-gray-50 dark:bg-black text-gray-900 dark:text-white transition-all duration-300">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/roadmap" element={<Roadmap />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
-  );
-}
-
-export default App;
+<Routes>
+  <Route path="/" element={<h1>Hello LYNX</h1>} />
+</Routes>
