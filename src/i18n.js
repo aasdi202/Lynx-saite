3c580fd87bdce04c4e992a7a16ab3349dfc25fe9
Ali <nf138992@gmail.com>
2025-05-03 16:20:12 +0330

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import translationEn from "../public/locales/en/translation.json";
-
+import translationEn from "./locales/en/translation.json";
 i18n
   .use(initReactI18next)
   .init({
