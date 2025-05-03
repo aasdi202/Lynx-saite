9862a8d072a59aadf480547648440ccfbc1bdccd
Ali <nf138992@gmail.com>
2025-05-03 19:17:12 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
-<Routes>
-  <Route path="/" element={<h1>Hello LYNX</h1>} />
-</Routes>
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
