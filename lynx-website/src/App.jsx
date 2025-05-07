825e488e166438b9db56aaf9aaf6d231b1df8b40
Ali <nf138992@gmail.com>
2025-05-07 21:32:48 +0330

    Update App.jsx

--- lynx-website/src/App.jsx
+++ lynx-website/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'; // مسیر دقیق فایل
-import Signup from './pages/Signup';
+import Signup from './pages/signup';
 
 function App() {
   return (
