6973845d6553195ff34ff99e77b3a78eb875cf18
Ali <nf138992@gmail.com>
2025-05-28 15:24:59 +0330

    pdated site with final design and font fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import './i18n';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; 
 import './index.css';
-import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
