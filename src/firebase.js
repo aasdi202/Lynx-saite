2e31307cdab82e30f7168975b81971c3595019ba
Ali <nf138992@gmail.com>
2025-05-30 20:33:43 +0330

    Updated site with final design and font fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,22 @@
+// Import the functions you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// Your web app's Firebase configuration (REAL CONFIG)
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "lynx-chat-4ceb9.firebaseapp.com",
-  projectId: "lynx-chat-4ceb9",
-  storageBucket: "lynx-chat-4ceb9.appspot.com",
-  messagingSenderId: "624341326911",
-  appId: "1:624341326911:web:d7dff4f5360e2c9410ec8a"
+  apiKey: "AIzaSyAhU-xzySzmkiYvLtLNpxiP91Ot07NQHg0",
+  authDomain: "helplynx-id20.firebaseapp.com",
+  projectId: "lynx-id20",
+  storageBucket: "lynx-id20.appspot.com",
+  messagingSenderId: "82098628112",
+  appId: "1:82098628112:web:a9e2c31de2f9b021204c95"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firestore and Auth
 const db = getFirestore(app);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
